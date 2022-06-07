47a68e973105417ca32673b6807fe38674ee99ce
Pramuditha Jayasinghe <pramuditha.jayasinghe@pearson.com>
2022-06-07 13:43:43 +0530

    Make active link using NavLink and styles prop

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,19 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
-  return (
-    <nav>
-        <NavLink to='/' >Home</NavLink>
-        <NavLink to='/about'>About</NavLink>
-    </nav>
-  )
+
+    const navLinkStyles = ({isActive}) => {
+        //props will receive isActive, hence destructure and use it
+        return {
+            fontWeight: isActive? 'bold' : 'normal',
+            textDecoration: isActive? 'none' : 'underline',
+        } 
+    }
+
+    return (
+        <nav>
+            <NavLink to='/' style={navLinkStyles}>Home</NavLink>
+            <NavLink to='/about' style={navLinkStyles}>About</NavLink>
+        </nav>
+    )
 }
