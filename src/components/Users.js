9f68e4764364be7c742ac09c0a0ff2741fcfab05
Pramuditha Jayasinghe <pramuditha.jayasinghe@pearson.com>
2022-06-08 13:05:30 +0530

    useSearchParams hooks to set dynamic fliters in url and make dynamic content

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useSearchParams } from 'react-router-dom'
 
 export const Users = () => {
-  return (
-    <div>
-        <h1>User1</h1>
-        <h1>User2</h1>
-        <h1>User3</h1>
-        <Outlet/>
-    </div>
-  )
+    const [searchParams, setSearchParams] = useSearchParams();
+    const showActiveUsers = searchParams.get('filter') === 'active';
+    return (
+        <div>
+            <h1>User1</h1>
+            <h1>User2</h1>
+            <h1>User3</h1>
+            <Outlet/>
+            <button onClick={() => setSearchParams({filter: 'active'})}>Show Active users</button>
+            <button onClick={() => setSearchParams({})}>Reset filter</button>
+
+           {
+                showActiveUsers ? (
+                    <h2>Showing active users</h2>
+                ) : (
+                    <h2>Showing all users</h2>
+                )
+           }
+        </div>
+    )
 }
