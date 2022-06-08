ef9ecfc73b29789c76ded472a350b18ef6c6d739
Pramuditha Jayasinghe <pramuditha.jayasinghe@pearson.com>
2022-06-08 12:31:49 +0530

    Dynamic routes implementation

---
+++ src/components/Users.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export const Users = () => {
+  return (
+    <div>
+        <h1>User1</h1>
+        <h1>User2</h1>
+        <h1>User3</h1>
+        <Outlet/>
+    </div>
+  )
+}
